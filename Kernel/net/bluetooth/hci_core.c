7cbc61b78184c5dfbfcfa90a49809fc5040c2f09
nubecoder <nubecoder@gmail.com>
2011-08-11 21:17:58 -0400

    Revert "Bluetooth: Make hci_send_sco() void"

--- Kernel/net/bluetooth/hci_core.c
+++ Kernel/net/bluetooth/hci_core.c
@@ -1279,12 +1279,17 @@ int hci_send_acl(struct hci_conn *conn, struct sk_buff *skb, __u16 flags)
 EXPORT_SYMBOL(hci_send_acl);
 
 /* Send SCO data */
-void hci_send_sco(struct hci_conn *conn, struct sk_buff *skb)
+int hci_send_sco(struct hci_conn *conn, struct sk_buff *skb)
 {
 	struct hci_dev *hdev = conn->hdev;
 	struct hci_sco_hdr hdr;
 
 	BT_DBG("%s len %d", hdev->name, skb->len);
+
+	if (skb->len > hdev->sco_mtu) {
+		kfree_skb(skb);
+		return -EINVAL;
+	}
 
 	hdr.handle = cpu_to_le16(conn->handle);
 	hdr.dlen   = skb->len;
@@ -1296,7 +1301,13 @@ void hci_send_sco(struct hci_conn *conn, struct sk_buff *skb)
 	skb->dev = (void *) hdev;
 	bt_cb(skb)->pkt_type = HCI_SCODATA_PKT;
 	skb_queue_tail(&conn->data_q, skb);
+<<<<<<< HEAD
 	hci_sched_tx(hdev);
+=======
+	tasklet_schedule(&hdev->tx_task);
+
+	return 0;
+>>>>>>> 635039c... Revert "Bluetooth: Make hci_send_sco() void"
 }
 EXPORT_SYMBOL(hci_send_sco);
 
