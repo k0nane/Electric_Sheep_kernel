acbab049e532f9370509d0ae7a2fb3439bf8474c
k0nane <k0@k0nane.info>
2011-09-15 19:09:17 -0500

    Committing to switch branch

--- Kernel/include/linux/autoconf.h
+++ Kernel/include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.32.9
- * Thu Sep 15 18:36:51 2011
+ * Thu Sep 15 18:42:00 2011
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_MMC_SDHCI_S3C_DMA 1
